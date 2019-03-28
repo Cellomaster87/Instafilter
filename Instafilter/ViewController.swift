481fc46102d541acd4fda572dab2682414022861
Michele Galvagno <michicello@gmail.com>
2019-03-28 18:55:02 +0100

    Designing the interface

--- Instafilter/ViewController.swift
+++ Instafilter/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: Outlets and Properties
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    
+    // MARK: Views management
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    // MARK: Actions
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
 }
 
