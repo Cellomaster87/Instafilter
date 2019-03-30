396c846b4b1bd1ea16950d8e1da23f9984863338
Michele Galvagno <michicello@gmail.com>
2019-03-30 15:12:54 +0100

    Challenge 3 completed (to be perfection!)

--- Instafilter/ViewController.swift
+++ Instafilter/ViewController.swift
@@ -12,7 +12,9 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     // MARK: Outlets and Properties
     @IBOutlet var imageView: UIImageView!
-    @IBOutlet var intensity: UISlider!
+    @IBOutlet var intensitySlider: UISlider!
+    @IBOutlet var radiusSlider: UISlider!
+    @IBOutlet var scaleSlider: UISlider!
     @IBOutlet var changeFilter: UIButton!
     var currentImage: UIImage!
     
@@ -92,9 +94,19 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     func applyProcessing() {
         let inputKeys = currentFilter.inputKeys
-        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey) }
-        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(intensity.value * 200, forKey: kCIInputRadiusKey) }
-        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(intensity.value * 10, forKey: kCIInputScaleKey) }
+        
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(intensitySlider.value, forKey: kCIInputIntensityKey)
+        }
+        
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(radiusSlider.value * 100, forKey: kCIInputRadiusKey)
+        }
+        
+        if inputKeys.contains(kCIInputScaleKey) {
+            currentFilter.setValue(scaleSlider.value * 10, forKey: kCIInputScaleKey)
+        }
+        
         if inputKeys.contains(kCIInputCenterKey) { currentFilter.setValue(CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2), forKey: kCIInputCenterKey) }
         
         guard let outputImage = currentFilter.outputImage else { return }
@@ -110,7 +122,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         guard let actionTitle = action.title else { return }
         
         currentFilter = CIFilter(name: actionTitle)
-//        changeFilter.titleLabel?.text = actionTitle
         
         let beginImage = CIImage(image: currentImage)
         currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
